11149765a847bd1d377b2f3fc742ea9e9551dc5d
Matt Carter <m@ttcarter.com>
2018-03-21 10:35:46 +1000

    Library now supports two main barcode readers - Quagga (free) and Bardecode (licensing required)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var async = require('async-chainable');
+var asyncExec = require('async-chainable-exec');
 var barcodeReader = require('quagga').default;
 var events = require('events');
 var fs = require('fs');
@@ -11,11 +12,21 @@ var util = require('util');
 /**
  * PDFDicer functions that gives the all functionality.
  */
-function PDFDicer() {
+function PDFDicer(options) {
 	var dicer = this;
 
-	dicer._defaults = {
-		reader: {
+	dicer.settings = _.defaults(options, {
+		areas: [
+			// Top-center area
+			{ top: "3%", right: "2%", left: "2%", bottom: "87" }
+		],
+		imageFormat: 'png',
+		magickOptions: {}, // Options passed to ImageMagick when converting pdf -> page output format
+		bardecode: {
+			bin: '/home/mc/Desktop/builds/bardecoder_8_1_1/bin/bardecode',
+			checkEvaluation: true,
+		},
+		quagga: { // Options passed to Quagga
 			numOfWorkers: 0, // Always 0 in Node backend
 			locate: false, // We have to indicate the location of the barcode
 			inputStream: {
@@ -33,41 +44,81 @@ function PDFDicer() {
 			pages: 1,
 			areas: 1,
 		},
-	};
+	});
 
-	dicer.new = () => new PDFDicer();
+	dicer.profile = name => {
+		switch (name) {
+			case 'quagga':
+				_.merge(dicer.settings, {
+					driver: 'quagga',
+					imageFormat: 'png',
+					magickOptions: {
+						'-quality': 100,
+						'-density': 150,
+						'-background': 'white',
+						'-alpha': 'remove',
+						'-gaussian-blur': '0x1',
+					},
+				});
+				break;
+			case 'bardecode':
+				_.merge(dicer.settings, {
+					driver: 'bardecode',
+					imageFormat: 'tif',
+					magickOptions: {},
+				});
+				break;
+			default:
+				throw new Error(`Unknown profile: ${name}`);
+		}
 
-	dicer.defaults = function(options) {
-		_.merge(this._defaults, options);
 		return this;
 	};
 
-	dicer.areas = function(areas) {
-		this._defaults.areas = areas;
+
+	/**
+	* Convenience function to quickly set a setting
+	* @param {string} key The key of the setting to set, dotted notation is supported
+	* @param {*} value The value to set to
+	* @return {Object} This chainable object
+	*/
+	dicer.set = (key, value) => {
+		_.set(dicer.settings, key, value);
+		return dicer;
+	};
+
+
+	/**
+	* Shortcut function to set the areas used by the split function
+	* @param {array} areas The areas array to set. Each option should have `top`, `right`, `left`, `bottom` which are either percentages or absolute values
+	* @return {Object} This chainable object
+	*/
+	dicer.areas = areas => {
+		_.merge(this.settings.areas, areas);
 		return this;
 	};
 
 	/**
 	 * Take an input file and split it into many PDF files based on configured options
-	 * @emits stage Fired at each stage of the process. ENUM: 'init', 'readPDF', 'readPages', 'extracted', 'loadRange', 'splitPDFWithScissors'
-	 * 							'splitPDFWithScissors' stage is thrown as many times as is needed.
-	 * @emits tempDir The temp dir used for storage
-	 * @emits pageConverted  Fired with each page and pageOffset extracted as they are extracted
-	 * @emits pagesConverted Fired with an array of all extracted page collection
-	 * @emits pageAnalyze Fired before each page gets analyzed with the page object and offset
-	 * @emits pageAnalyzed Fired after each page has been analyzed with the page object and offset
-	 * @emits pagesAnalyzed Fired after all pages have been analyzed with the page collection
-	 * @emits rangeExtracted Fired after the splitted range has been calculated from the original input pdf
-	 * @emits split Fired after each pdf has been separated in the range and it gives you the pdf resultant
-	 * @emits splitted Fired after pdf have been separated in the range
 	 * @param {string} input The input path to process (ends in .pdf usually)
 	 * @param {Object} [options] Optional options which override the defaults
 	 * @param {function} callback Callback function to fire on completion or error
 	 * @return {PDFDicer} This chainable object
+	 *
+	 * @emits stage Fired at each stage of the process. ENUM: 'init', 'readPDF', 'readPages', 'extracted', 'loadRange', 'splitPDFWithScissors', 'splitPDFWithScissors' stage is thrown as many times as is needed.
+	 * @emits tempDir The temp dir used for storage
+	 * @emits pageConverted  Fired with each page and pageOffset extracted as they are extracted
+	 * @emits pagesConverted Fired with an array of all extracted page collection
+	 * @emits pageAnalyze Fired before each page gets analyzed with the page object
+	 * @emits pageAnalyzed Fired after each page has been analyzed with the page object
+	 * @emits pagesAnalyzed Fired after all pages have been analyzed with the page collection
+	 * @emits rangeExtracted Fired after the splitted range has been calculated from the original input pdf
+	 * @emits split Fired after each pdf has been separated in the range and it gives you the pdf resultant
+	 * @emits splitted Fired after pdf have been separated in the range
 	 */
 	dicer.split = function(input, options, callback) {
 		// Argument mangling {{{
-		if (_.isFunction(options)) { // input, options
+		if (_.isFunction(options)) { // Called as: input, callback
 			callback = options;
 			options = {};
 		} else if (!_.isFunction(callback)) {
@@ -75,7 +126,7 @@ function PDFDicer() {
 		}
 		// }}}
 
-		var settings = _.defaults({}, options, this._defaults);
+		var settings = _.defaults({}, options, this.settings);
 
 		async()
 			// Sanity checks {{{
@@ -101,14 +152,14 @@ function PDFDicer() {
 				dicer.emit('tempDir', this.tempDir);
 				next(null, new pdfImage(input, {
 					outputDirectory: this.tempDir,
-					convertOptions: {
-						'-quality': 100,
-						'-density': 150,
-						'-background': 'white',
-						'-alpha': 'remove',
-						'-gaussian-blur': '0x1'
-					},
+					convertOptions: settings.magickOptions,
 				}));
+			})
+			// }}}
+			// Set output image format {{{
+			.then(function(next) {
+				this.pdf.setConvertExtension(settings.imageFormat);
+				next();
 			})
 			// }}}
 			// Extract info about the input file {{{
@@ -119,7 +170,7 @@ function PDFDicer() {
 					.then(data => next(null, data), next)
 			})
 			// }}}
-			// Convert the file into PNG images {{{
+			// Convert the file into images {{{
 			.set('pages', {})
 			.then(function(next) {
 				dicer.emit('stage', 'readPages');
@@ -150,26 +201,51 @@ function PDFDicer() {
 			// Read each page and extract a barcode if there is one {{{
 			.limit(settings.threads.pages)
 			.forEach('pages', function(nextPage, page, pageOffset) {
-				dicer.emit('pageAnalyze', page, pageOffset)
+				page.number = parseInt(pageOffset) + 1;
 				page.barcode = false;
-				async()
-					.limit(settings.threads.areas)
-					.forEach(settings.areas, function(nextArea, area) {
-						barcodeReader.decodeSingle(_.merge({}, settings.reader, {
-							src: page.path,
-							inputStream: {
-								area: area,
-							},
-						}), function(res) {
-							if (!page.barcode && res && res.codeResult) page.barcode = res.codeResult.code;
-							nextArea();
-						});
-					})
-					.then(function(next) {
-						dicer.emit('pageAnalyzed', page, pageOffset);
-						next();
-					})
-					.end(nextPage);
+				dicer.emit('pageAnalyze', page)
+				if (settings.driver == 'quagga') {
+					async()
+						.limit(settings.threads.areas)
+						.forEach(settings.areas, function(nextArea, area) {
+							barcodeReader.decodeSingle(Object.assign({}, settings.quagga, {
+								src: page.path,
+								inputStream: {
+									area: area,
+								},
+							}), function(res) {
+								if (!page.barcode && res && res.codeResult) page.barcode = res.codeResult.code;
+								nextArea();
+							});
+						})
+						.then(function(next) {
+							dicer.emit('pageAnalyzed', page);
+							next();
+						})
+						.end(nextPage);
+				} else if (settings.driver == 'bardecode') {
+					async()
+						.use(asyncExec)
+						.exec('response', [settings.bardecode.bin, page.path])
+						.then('response', function(next) {
+							var firstExtracted = _(this.response)
+								.filter(i => ! /^EVALUATION MODE/.test(i))
+								.first();
+
+							if (firstExtracted) page.barcode = firstExtracted;
+
+							if (settings.bardecode.checkEvaluation && /\?\?\?$/.test(page.barcode)) console.warn('You are using an evaluation version of Bardecode - the last three digits of the barcode are obscured!');
+
+							next();
+						})
+						.then(function(next) {
+							dicer.emit('pageAnalyzed', page);
+							next();
+						})
+						.end(nextPage);
+				} else {
+					throw new Error(`Unknown page decoder: ${settings.driver}`);
+				}
 			})
 			// }}}
 			// Post pages analyzed emitters {{{
@@ -247,6 +323,8 @@ function PDFDicer() {
 
 		return this;
 	};
+
+	dicer.profile('quagga'); // Load default profile
 };
 
 util.inherits(PDFDicer, events.EventEmitter);
